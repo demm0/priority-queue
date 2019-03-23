5f363b69064c5869ce5207eca1d1bad59fa9af44
demm0 <47791149+demm0@users.noreply.github.com>
2019-03-24 00:09:17 +0300

    edit
    edit

--- src/queue.js
+++ src/queue.js
@@ -1,73 +1,75 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize = 30) {
-		this.items = [];
-		this.maxSize = maxSize;
-		this.heap = new MaxHeap();
+constructor(maxSize = 30) {
+this.maxSize = maxSize;
+this.heap = new MaxHeap();
 
-	}
+}
 
-	push(data, priority) {
-		if(this.size() == this.maxSize) {
-			throw "indexOutOfRangeException";
-		}
+push(data, priority) {
+if(this.size() == this.maxSize) {
+throw "";
+}
 
-		this.heap.push(data, priority);
-		
-		// if(this.items != 0) {
-		// 	for(let i = 0; i < this.items.length ; i++ ) {
-		// 		if(this.items[i][0] == priority)
-		// 		{
-		// 			this.items[i].push(data);
-		// 			return;
-		// 		}
-		// 	}
-		// }
+this.heap.push(data, priority);
 
-		
-		
-		// this.items.push([priority, data]);
-		
-	}
+// if(this.items != 0) {
+// for(let i = 0; i < this.items.length ; i++ ) {
+// if(this.items[i][0] == priority)
+// {
+// this.items[i].push(data);
+// return;
+// }
+// }
+// }
 
-	shift() {
-		return this.heap.size();
-		// let max_priority = -1;
-		// let index = -1;
-		
-		// for(let i = 0; i < this.items.length; i++) {
-		// 	if(this.items[i][0] > max_priority) {
-		// 		max_priority = this.items[i][0];
-		// 		index = i;
-		// 	}
-		// }
-		
-		// let element = this.items[index].splice(1, 1);
-		
-		// if(this.items[index].length == 1) {
-		// 	this.items.splice(index, 1);
-		// }
 
-		// return element;	
-	}
 
-	size() {
-		return this.heap != null ? this.heap.size() : 0;
-		// let length = 0;
+// this.items.push([priority, data]);
 
-		// if(this.items.length != 0) {
-		// 	for(let i = 0; i < this.items.length; i++) {
-		// 		length += this.items[i].length - 1;
-		// 	}
-		// }
+}
 
-		// return length;
-	}
+shift() {
+if(this.isEmpty()) {
+throw "";
+}
+return this.heap.pop();
+// let max_priority = -1;
+// let index = -1;
 
-	isEmpty() {
-		return this.size() == 0;	
-	}
+// for(let i = 0; i < this.items.length; i++) {
+// if(this.items[i][0] > max_priority) {
+// max_priority = this.items[i][0];
+// index = i;
+// }
+// }
+
+// let element = this.items[index].splice(1, 1);
+
+// if(this.items[index].length == 1) {
+// this.items.splice(index, 1);
+// }
+
+// return element;	
+}
+
+size() {
+return this.heap != null ? this.heap.size() : 0;
+// let length = 0;
+
+// if(this.items.length != 0) {
+// for(let i = 0; i < this.items.length; i++) {
+// length += this.items[i].length - 1;
+// }
+// }
+
+// return length;
+}
+
+isEmpty() {
+return this.size() == 0;	
+}
 }
 
 module.exports = PriorityQueue;
